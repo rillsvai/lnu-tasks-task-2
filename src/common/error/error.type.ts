16cd99f8b8d09ba81fc9828513e782440f13c727
RillSvai <kirilkvas07@gmail.com>
2024-09-07 10:45:22 +0300

    remove redundancy

--- src/common/error/error.type.ts
+++ src/common/error/error.type.ts
@@ -1,7 +1,1 @@
-import { Response } from "express";
-
 export type AppValidationError = Record<string, string>;
-
-/* eslint-disable */
-export type AppControllerFunction<TReq> = (req: TReq, res: Response) => any;
-/* eslint-enable */
