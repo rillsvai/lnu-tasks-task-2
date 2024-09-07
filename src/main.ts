c6872eb86eff9acda8952bd1330f1feed3c2f284
RillSvai <kirilkvas07@gmail.com>
2024-09-07 12:12:18 +0300

    add rate limiter

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,25 @@ import express from 'express';
 import { appRouter } from './app.router';
 import { appLogger } from './common/logger/logger.config';
 import { Server } from 'http';
+import rateLimit from 'express-rate-limit';
+import morgan from 'morgan';
+
+export const apiLimiter = rateLimit({
+  windowMs: 10 * 1000,
+  max: 1000,
+  message: {
+    status: 429,
+    message: 'Too many requests, please try again later.',
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 export const app: Application = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
+app.use(apiLimiter);
 app.use(appRouter);
 
 appLogger.info(`starting server on port: ${8080} `);
--- src/tests/weather/weather.load.js
+++ src/tests/weather/weather.load.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  stages: [
-    { duration: '4s', target: 100 },
-    { duration: '4s', target: 100 },
-    { duration: '4s', target: 0 },
-  ],
+  stages: [{ duration: '4s', target: 120 }],
+  thresholds: {
+    http_req_duration: ['p(95)<500'],
+    http_req_failed: ['rate<0.01'],
+  },
 };
 
 export default function () {
